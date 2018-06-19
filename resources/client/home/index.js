d7111497953bb400b085d247fea533720591cfce
Andrew Schorr <afs35mm@gmail.com>
2018-06-18 23:38:33 -0400

    moved dom to JS

--- resources/client/home/index.js
+++ resources/client/home/index.js
@@ -1,1 +1,13 @@
-console.log(window.DATA_BS);
+import React from 'react';
+import ReactDOM from 'react-dom';
+// console.log(window.DATA_BS);
+if (module.hot) {
+    module.hot.accept();
+}
+const App = () => (
+    <div>
+        <p>im da hi!</p>
+    </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
