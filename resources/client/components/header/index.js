ee3514638bca537f0bf612b70e8b94021fb524fe
Andrew Schorr <afs35mm@gmail.com>
2018-06-21 09:22:13 -0400

    just some tiny work

--- resources/client/components/header/index.js
+++ resources/client/components/header/index.js
@@ -31,27 +31,26 @@ export default class Header extends React.Component {
         return (
             <div>
                 <Navbar color="light" light expand="md">
-                    <NavbarBrand href="/">reactstrap</NavbarBrand>
+                    <NavbarBrand href="/">lo</NavbarBrand>
                     <NavbarToggler onClick={this.toggle} />
                     <Collapse isOpen={this.state.isOpen} navbar>
                         <Nav className="ml-auto" navbar>
-                            <NavItem>
+                            {/* <NavItem>
                                 <NavLink href="/components/">Components</NavLink>
                             </NavItem>
                             <NavItem>
                                 <NavLink href="https://github.com/reactstrap/reactstrap">
                                     GitHub
                                 </NavLink>
-                            </NavItem>
+                            </NavItem> */}
                             <UncontrolledDropdown nav inNavbar>
                                 <DropdownToggle nav caret>
-                                    Options
+                                    Name of Logged In Person
                                 </DropdownToggle>
                                 <DropdownMenu right>
-                                    <DropdownItem>Option 1</DropdownItem>
-                                    <DropdownItem>Option 2</DropdownItem>
+                                    <DropdownItem tag="a" href="/account">Settings</DropdownItem>
                                     <DropdownItem divider />
-                                    <DropdownItem>Reset</DropdownItem>
+                                    <DropdownItem tag="a" href="/logout">Logout</DropdownItem>
                                 </DropdownMenu>
                             </UncontrolledDropdown>
                         </Nav>
