de0cfa934c09376ae87f94710b92fddb20b4b269
Andrew Schorr <afs35mm@gmail.com>
2018-06-22 09:59:25 -0400

    idk

--- resources/client/home/profile/index.js
+++ resources/client/home/profile/index.js
@@ -1,6 +1,28 @@
 import React, { Component } from 'react';
 
+// Here we run a very simple test of the Graph API after login is
+// successful.  See statusChangeCallback() for when this call is made.
+function testAPI() {
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', response => {
+        console.log(`Successful login for: ${response.name}`);
+        document.getElementById('status').innerHTML = `Thanks for logging in, ${response.name}!`;
+    });
+}
+
 class Profile extends Component {
+    componentDidMount() {
+        (function(d, s, id) {
+            let js,
+                fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s);
+            js.id = id;
+            js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8&appId=160693578124765";
+            fjs.parentNode.insertBefore(js, fjs);
+        })(document, 'script', 'facebook-jssdk');
+    }
+
     render() {
         return (
             <div className="col-sm-9">
@@ -56,6 +78,16 @@ class Profile extends Component {
                                     </p>
                                 </small>
                             </div>
+                            <div className="form-group">
+                                <div
+                                    className="fb-login-button"
+                                    data-size="medium"
+                                    data-auto-logout-link="true"
+                                    data-onlogin="checkLoginState();"
+                                />
+                                <div id="status" />
+                            </div>
+
                             <br />
                             <button type="submit" className="btn btn-primary">
                                 Save
